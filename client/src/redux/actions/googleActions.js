9fe60bd1e547315ef5d940527f629e6b8af2ace9
HEESU LEE <cutterballer@gmail.com>
2022-04-07 19:03:34 +0900

    Clientside : Update googleActions.js credential
    Clientside : Update googleActions.js post withcredentials true

--- client/src/redux/actions/googleActions.js
+++ client/src/redux/actions/googleActions.js
@@ -7,7 +7,7 @@ export const googleStart = (tokenId) => async (dispatch)=> {
     const id_token = tokenId;
     console.log("idToken",id_token)
     let d = await axios
-      .post(`${process.env.REACT_APP_API_URL}/auth/auth`, { id_token })
+      .post(`${process.env.REACT_APP_API_URL}/auth/auth`, { id_token },{ withCredentials: true })
       .then((res) => {
         console.log("res1",res)
         if (res.data.message !== "ok") {
