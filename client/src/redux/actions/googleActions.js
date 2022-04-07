2b8566218c8a5b6c272a50c128afd2553e6684db
yoon0321 <wodmstl@gmail.com>
2022-04-07 16:49:53 +0900

    client 수정

--- client/src/redux/actions/googleActions.js
+++ client/src/redux/actions/googleActions.js
@@ -1,28 +1,24 @@
-import { GOOGLE_START } from "../types";
-import { LOGIN_START } from "../types";
+import { GOOGLE_START } from '../types';
+import { LOGIN_START } from '../types';
 import axios from 'axios';
 
-
-export const googleStart = (tokenId) => async (dispatch)=> {
-    const id_Token = tokenId;
-    console.log("idToken",id_Token)
-    let d = await axios
-      .post(`${process.env.REACT_APP_API_URL}/user/oauth`, { id_Token })
-      .then((res) => {
-        console.log("res1",res)
-        if (res.data.message !== "ok") {
-          return false;
-        } else {
-            localStorage.setItem("accessToken", res.data.data.accessToken);
-            const c = axios
-            .post(`${process.env.REACT_APP_API_URL}/users/auth`,
-              { accessToken: res.data.data.accessToken },
-              { withCredentials: true }
-            ).then((userinformation)=>{
-                console.log("userinformation",userinformation);
-                dispatch({type:LOGIN_START,payload:userinformation.data.data.userInfo});
-                return true
-            })
-        }
-    }
-)};
+export const googleStart = (tokenId) => async (dispatch) => {
+  const id_token = tokenId;
+  console.log('idToken', id_token);
+  let d = await axios
+    .post(
+      `${process.env.REACT_APP_API_URL}/auth/auth`,
+      { id_token },
+      { withCredentials: true }
+    )
+    .then((res) => {
+      console.log('res1', res);
+      if (res.data.message !== 'ok') {
+        return false;
+      } else {
+        localStorage.setItem('accessToken', res.data.data.accessToken);
+        dispatch({ type: LOGIN_START, payload: res.data.data.userInfo });
+        return true;
+      }
+    });
+};
