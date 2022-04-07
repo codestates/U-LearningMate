aea63075e9514d299b5a6506a103b72304ac792e
yoon0321 <wodmstl@gmail.com>
2022-04-07 18:04:22 +0900

    로그인 모달창 수정

--- client/src/redux/actions/googleActions.js
+++ client/src/redux/actions/googleActions.js
@@ -1,24 +1,20 @@
-import { GOOGLE_START } from '../types';
-import { LOGIN_START } from '../types';
+import { GOOGLE_START } from "../types";
+import { LOGIN_START } from "../types";
 import axios from 'axios';
 
-export const googleStart = (tokenId) => async (dispatch) => {
-  const id_token = tokenId;
-  console.log('idToken', id_token);
-  let d = await axios
-    .post(
-      `${process.env.REACT_APP_API_URL}/auth/auth`,
-      { id_token },
-      { withCredentials: true }
-    )
-    .then((res) => {
-      console.log('res1', res);
-      if (res.data.message !== 'ok') {
-        return false;
-      } else {
-        localStorage.setItem('accessToken', res.data.data.accessToken);
-        dispatch({ type: LOGIN_START, payload: res.data.data.userInfo });
-        return true;
-      }
-    });
-};
+
+export const googleStart = (tokenId) => async (dispatch)=> {
+    const id_token = tokenId;
+    console.log("idToken",id_token)
+    let d = await axios
+      .post(`${process.env.REACT_APP_API_URL}/auth/auth`, { id_token })
+      .then((res) => {
+        console.log("res1",res)
+        if (res.data.message !== "ok") {
+          return false;
+        } else {
+            localStorage.setItem("accessToken", res.data.data.accessToken);
+            dispatch({type:LOGIN_START,payload:res.data.data.userInfo});
+            return true
+    }
+  })};
