5324295c92b72963d2b006d85e2e55badeb1e405
yoon0321 <wodmstl@gmail.com>
2022-04-08 17:38:23 +0900

    auth코드 수정

--- server/controllers/auth/auth.js
+++ server/controllers/auth/auth.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { user } = require('../../models');
+const { User } = require('../../models');
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
@@ -11,15 +11,20 @@ module.exports = async (req, res) => {
   // res.status(200).send('hi!');
   // console.log('auth 되냐?');
   const token = req.body.id_token;
+  console.log(token);
   const ticket = await client.verifyIdToken({
-    idToken: token,
+    idToken: token, // 잘못된 값이 들어왔을 경우를 대비한 예외처리코드 작성하기(20220407)
     audience: process.env.GOOGLE_CLIENT_ID,
   });
 
+  console.log(ticket);
   const payload = ticket.getPayload(); // 데이터 핸들러
   const email = payload['email']; // 데이터 핸들러
+  if (!payload) {
+    res.status(400).send('no token');
+  }
 
-  const [find, created] = await user.findOrCreate({
+  const [find, created] = await User.findOrCreate({
     where: {
       email: email,
     },
@@ -41,3 +46,5 @@ module.exports = async (req, res) => {
   delete data.dataValues.password;
   res.status(200).json({ userInfo: data.dataValues });
 };
+
+// http://ec2-13-125-238-251.ap-northeast-2.compute.amazonaws.com:8080
