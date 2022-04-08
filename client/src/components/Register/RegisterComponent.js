5f805d47118aff58ed0a5435f07a1af1433d22a4
HEESU LEE <cutterballer@gmail.com>
2022-04-08 10:56:38 +0900

    Clientside: RegisterCompon delete mobilelevelteam
    Clientside: RegisterComponent delete mobile,level,team

--- client/src/components/Register/RegisterComponent.js
+++ client/src/components/Register/RegisterComponent.js
@@ -39,8 +39,8 @@ const RegisterComponent = () => {
     e.preventDefault();
     // setState({ email: "", password: "" });
     
-    dispatch(signupStart(email,nickname,password,mobile,level,team));
-    // console.log("이것만 써도 될것같은데 왜 디스패치쓰지?",signupStart(email,nickname,password,mobile,level,team))
+    dispatch(signupStart(email,nickname,password));
+    // console.log("이것만 써도 될것같은데 왜 디스패치쓰지?",signupStart(email,nickname,password))
   };
 
   return (
