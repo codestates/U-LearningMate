636670e2cdb98f81493611eb3414b9c8ecadda28
hyocy <hyocy0914@gmail.com>
2022-04-11 23:11:27 +0900

    Update signup.js

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -19,17 +19,12 @@ module.exports = {
     } catch (err) {
         console.log(err);
         res.status(500).send();
-      }    
-    //res.status(200).send('signup');
+      }        
   }
 }
-// 1. req 에서 데이터 받아오기 v
-// 2. 받아온 데이터에 나머지 데이터 채워넣어서 (oauth) v
-// 3. sequelize 통해서 DB에 입력하기 v
 // *. email, nickname 중복 확인 엔드포인트 필요 >
 // * 비밀번호 암호화에 대해서 고민해보기 >
 // !. Users 테이블에 oauth 컬럼 추가 (integer) v
     // 0: 자체 , 1: google, 2: daum
-// !. score 컬럼 자동 생성으로 변경 v
 // 클라이언트에서 토큰확인 후 닉네임 넣어서 오는 방법 고민  >
 // ! 주석처리 한 부분으로 바꿔도 작동하는지 확인
